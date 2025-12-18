57734396e01b816c6b3f035cda654ac7c3813082
harpreet-2146 <harpreetk2146@gmail.com>
2025-12-18 20:48:29 +0530

    Update ExpenseService to implement interface

--- expense/src/main/java/com/eclipseX/expense/service/ExpenseService.java
+++ expense/src/main/java/com/eclipseX/expense/service/ExpenseService.java
@@ -1,33 +1,37 @@
 package com.eclipseX.expense.service;
 
 import com.eclipseX.expense.model.Expense;
-import com.eclipseX.expense.repository.ExpenseRepository;
+import com.eclipseX.expense.repository.IExpenseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ExpenseService {
+public class ExpenseService implements IExpenseService {
 
-    private final ExpenseRepository expenseRepository;
+    private final IExpenseRepository expenseRepository;
 
-    public ExpenseService(ExpenseRepository expenseRepository) {
+    public ExpenseService(IExpenseRepository expenseRepository) {
         this.expenseRepository = expenseRepository;
     }
 
+    @Override
     public List<Expense> getAllExpenses() {
         return expenseRepository.findAll();
     }
 
+    @Override
     public Optional<Expense> getExpenseById(Long id) {
         return expenseRepository.findById(id);
     }
 
+    @Override
     public Expense saveExpense(Expense expense) {
         return expenseRepository.save(expense);
     }
 
+    @Override
     public void deleteExpense(Long id) {
         expenseRepository.deleteById(id);
     }
